420d34dad6ae02ae2d027151e4a51c49b2be6cde
mthoolen <mjjthoolen@gmail.com>
2018-12-27 12:27:29 +0100

    frappe controller set up

--- frappe-controller/src/frappecontroller.go
+++ frappe-controller/src/frappecontroller.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rstraub/dontworrybefrappe/frappe_contracts"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
 
+// main start a gRPC server and waits for connection
 func main() {
-	fmt.Print("Hello World!")
+	// create a listener on TCP port 7777
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// create a server instance
+	s := frappe_contracts.Server{}
+	// create a gRPC server object
+	grpcServer := grpc.NewServer()
+	// attach the Ping service to the server
+	frappe_contracts.RegisterPingServer(grpcServer, &s)
+	// start the server
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+	}
 }
