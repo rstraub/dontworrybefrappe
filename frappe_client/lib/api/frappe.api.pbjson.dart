0b2e90467877552b54beb55c332384466d7656a7
mthoolen <mjjthoolen@gmail.com>
2018-12-28 14:32:22 +0100

    left behind a single sad dart file

---
+++ frappe_client/lib/api/frappe.api.pbjson.dart
@@ -0,0 +1,31 @@
+///
+//  Generated code. Do not modify.
+//  source: frappe.api.proto
+///
+// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
+
+const CoffeeType$json = const {
+  '1': 'CoffeeType',
+  '2': const [
+    const {'1': 'BLACK', '2': 0},
+    const {'1': 'CAPPUCCINO', '2': 1},
+    const {'1': 'ESPRESSO', '2': 2},
+  ],
+};
+
+const CoffeeOrder$json = const {
+  '1': 'CoffeeOrder',
+  '2': const [
+    const {'1': 'type', '3': 1, '4': 1, '5': 14, '6': '.frappe_api.CoffeeType', '10': 'type'},
+  ],
+};
+
+const Coffee$json = const {
+  '1': 'Coffee',
+  '2': const [
+    const {'1': 'type', '3': 1, '4': 1, '5': 14, '6': '.frappe_api.CoffeeType', '10': 'type'},
+    const {'1': 'sugar', '3': 2, '4': 1, '5': 8, '10': 'sugar'},
+    const {'1': 'strength', '3': 3, '4': 1, '5': 5, '10': 'strength'},
+  ],
+};
+
